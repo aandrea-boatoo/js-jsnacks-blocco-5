ffef41cbfafb5e8d29865c8fa9d51a367d4ee1a4
Boato Andrea <163646394+aandrea-boatoo@users.noreply.github.com>
2024-11-15 15:38:05 +0100

    snack 8

--- ex-js-array-methods-main/snack-8/script.js
+++ ex-js-array-methods-main/snack-8/script.js
@@ -21,3 +21,7 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+const lanciMarco = students.find(element => element.name = "Marco Lanci");
+console.log(lanciMarco);
+const lanciMClass = lanciMarco.class;
+console.log(lanciMClass)
