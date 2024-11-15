883146e4da44e2cf0b7176df0e7f4fc03754bbf7
Boato Andrea <163646394+aandrea-boatoo@users.noreply.github.com>
2024-11-15 17:54:52 +0100

    fixed snack 3

--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -2,8 +2,5 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
-const numbersPlus = numbers.map((number) => {
-    number += 1;
-    return number
-})
+const numbersPlus = numbers.map((number) => number += 1);
 console.log(numbersPlus);
